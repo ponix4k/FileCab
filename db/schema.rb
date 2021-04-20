08a8610c7b51f512e82cbaf5969b657b59b32b00
John Williams <john.williams@kane.co.uk>
2021-04-20 16:59:30 +0100

    Add user id

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_20_151135) do
+ActiveRecord::Schema.define(version: 2021_04_20_152005) do
 
   create_table "docs", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
