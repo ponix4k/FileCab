a0abd9f7a2e806daa2fe5892620145831cc584cb
John Williams <john.williams@kane.co.uk>
2021-05-13 16:11:19 +0100

    change index to only show users docs

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -2,18 +2,18 @@ class DocsController < ApplicationController
   before_action :find_doc, only: [:show, :edit, :update, :destroy]
 
   def index
-    @docs = Doc.all.order("created_at DESC")
+    @docs = Doc.where(user_id: current_user)
   end
 
   def show
   end
 
   def new
-    @doc = Doc.new
+    @doc = current_user.docs.build
   end
 
   def create
-    @doc = Doc.new(doc_params)
+    @doc = current_user.docs.build(doc_params)
 
     if @doc.save
       redirect_to @doc
--- db/migrate/20210513144022_create_users.rb
+++ db/migrate/20210513144022_create_users.rb
@@ -1,15 +1,13 @@
-# frozen_string_literal: true
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
 
-class AddDeviseToUsers < ActiveRecord::Migration[6.1]
-  def self.up
-    change_table :users do |t|
-      ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      t.string :reset_password_token
+      t.datetime :rest_password_sent_at
 
       ## Rememberable
       t.datetime :remember_created_at
@@ -37,6 +35,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[6.1]
       # t.timestamps null: false
     end
 
+
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
@@ -47,5 +46,6 @@ class AddDeviseToUsers < ActiveRecord::Migration[6.1]
     # By default, we don't want to make any assumption about how to roll back a migration when your
     # model already existed. Please edit below which fields you would like to remove in this migration.
     raise ActiveRecord::IrreversibleMigration
+
   end
 end
