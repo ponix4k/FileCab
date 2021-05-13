eeb4a5b80d6b2e2b828177c213863304a750dccb
John Williams <john.williams@kane.co.uk>
2021-05-13 16:17:27 +0100

    change route for authenticated user

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
 
+  resources :docs
+
+  authenticated :user do
+    root "docs#index", as: "authenticated_root"
+  end
+
   root 'welcome#index'
 
-  resources :docs
 end
